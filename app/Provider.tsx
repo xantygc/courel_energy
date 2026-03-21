0915d44d5c3a252d4d2c279d90525c9b0d061993
Santiago González Courel <santiago.gonzalez.courel@gmail.com>
2026-03-21 18:13:28 +0100

    feat(onboarding): add nextstepjs tutorial for new users

--- app/Provider.tsx
+++ app/Provider.tsx
@@ -1,7 +1,12 @@
-"use client";
-
 import { SessionProvider } from "next-auth/react";
+import { NextStepProvider } from "nextstepjs";
 
 export default function Provider({ children }: { children: React.ReactNode }) {
-  return <SessionProvider>{children}</SessionProvider>;
+  return (
+    <SessionProvider>
+      <NextStepProvider>
+        {children}
+      </NextStepProvider>
+    </SessionProvider>
+  );
 }
